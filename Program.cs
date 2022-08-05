6cc0f1b1eda7717c1be73e009cce46dd05e3e382
Shred1 <shredder842@gmail.com>
2022-08-05 08:27:44 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace assignment_sorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 2,3,1,6,4,7,5,9,8};
+            int x;
+            for (int i = 0; i < numbers.Length - 1 ; i++)
+            {
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] < numbers[j])
+                    {
+                        x = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = x; 
+                    }
+                }
+
+            }
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            
+
+            
+        }
+    }
+}
